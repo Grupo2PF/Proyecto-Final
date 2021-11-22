496c807f3124e4a2af2875ad65688dd672a903ee
KannonH2 <manax.punk182@gmail.com>
2021-11-22 19:20:32 -0300

    fixed gitignore

--- front/src/redux/store/index.ts
+++ front/src/redux/store/index.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import {applyMiddleware, createStore} from "redux"
+// @ts-ignore
 import {composeWithDevTools} from "redux-devtools-extension"
+// @ts-ignore
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
