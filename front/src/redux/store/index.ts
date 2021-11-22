b222691f9546d5795f5c340dee51f31835865bc7
KannonH2 <manax.punk182@gmail.com>
2021-11-22 19:20:54 -0300

    Revert "fixed gitignore"
    This reverts commit 496c807f3124e4a2af2875ad65688dd672a903ee.

--- front/src/redux/store/index.ts
+++ front/src/redux/store/index.ts
@@ -1,8 +1,5 @@
-// @ts-ignore
 import {applyMiddleware, createStore} from "redux"
-// @ts-ignore
 import {composeWithDevTools} from "redux-devtools-extension"
-// @ts-ignore
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
