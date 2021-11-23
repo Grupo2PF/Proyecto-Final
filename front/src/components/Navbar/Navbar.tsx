c8c4821e5ccebb4fc59fb5ca1b8ac11b05efcfaf
Esteban Luna <estebanlun14777@gmail.com>
2021-11-23 17:34:18 -0300

    PreMerge

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,17 @@
+import  styles  from "./Navbar.module.scss"
+import logo from "../../assets/logo/dev-sky-black-logo.svg"
+
 export default function Navbar() {
     return (
         <div>
-            Navbar
+            <nav className={styles.navContainer}>
+                <img src={logo} alt="" className={styles.logo} />
+                <ul className={styles.ul}>
+                    <li className={styles.li}>
+                        <a href="/sesion" className={styles.a} > Iniciar Sesion </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
