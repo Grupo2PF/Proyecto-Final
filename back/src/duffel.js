d39ef85776ad3dcb98534ca2615236675be7c004
BauKSA <86334428+BauKSA@users.noreply.github.com>
2021-11-23 13:50:13 -0300

    iata json

--- back/src/duffel.js
+++ back/src/duffel.js
@@ -1,7 +1,9 @@
-import { Duffel } from '@duffel/api'
+const { Duffel } = require('@duffel/api');
+
 
 const duffel = new Duffel({
-    token: duffel_test_5oKRr362CbQ5GAv-enslSuMIgYqXC9nrvaFBVSFbYEi,
+    token: "duffel_test_5oKRr362CbQ5GAv-enslSuMIgYqXC9nrvaFBVSFbYEi",
 })
 
-export default duffel;
+console.log(duffel.token);
+module.exports = duffel;
