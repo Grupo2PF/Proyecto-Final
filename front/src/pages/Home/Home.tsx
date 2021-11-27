530188d2bfdc285b24edf760b446178c395fad66
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-11-26 21:21:11 -0400

    Update Home.tsx

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Home.module.scss";
-import SearchBar from "../../components/Searchbar/Searchbar";
+// import SearchBar from "../../components/Searchbar/Searchbar";
 import PopularDestinations from "../../components/PopularDestinations/PopularDestinations";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Navbar />
       <div className={style.aux} id="#home-top">
-        <SearchBar />
+        {/* <SearchBar /> */}
 
         <PopularDestinations />
 
