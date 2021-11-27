d71aac7da8c51969720ca99257cf32bff4475dc0
gonzaqepasa <gonzalovam@hotmail.com>
2021-11-27 00:20:30 -0300

    Revert "commit de prueba no mergear"
    This reverts commit ce716f8855349798aae7e920d1ff38fd714b8fce, reversing
    changes made to d6ee159d8811e09acf5f50d38fe31af72cf58236.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,33 @@
-import { Switch, Route } from "react-router";
-import "./App.css";
-// import Footer from "./components/Footer/Footer";
-// import Navbar from "./components/Navbar/Navbar";
-import AboutUs from "./pages/AboutUs/AboutUs";
-import Home from "./pages/Home/Home";
-import LoginPage from "./pages/LoginPage/LoginPage";
+import { Switch, Route } from 'react-router';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import PopularDestinations from './components/PopularDestinations/PopularDestinations';
+import SearchBar from './components/Searchbar/Searchbar';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about-us" component={AboutUs} />
-        <Route exact path="/login" component={LoginPage} />
+
+
+        <Route exact path='/'>
+          <Home />
+          <PopularDestinations />
+          <Footer />
+
+        </Route>
+
+        <Route exact path='/about-us'>
+          <AboutUs />
+         
+          <Footer />
+
+        </Route>
+
       </Switch>
     </div>
   );
