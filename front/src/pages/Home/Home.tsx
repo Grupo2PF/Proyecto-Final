82750822d6257f0d0f3b44087e04068651f23993
KannonH2 <manax.punk182@gmail.com>
2021-11-29 14:56:22 -0300

    Funcionando 2

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import SearchBar from '../../components/Searchbar/Searchbar'
 import style from './Home.module.scss'
+import PopularDestinations from "../../components/PopularDestinations/PopularDestinations";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 
 
 
 export default function Home() {
     return (
         <div className={style.allHome}>
+            <Navbar/>
+
             <SearchBar />
+
+            <PopularDestinations />
+
 
         </div>
     )
