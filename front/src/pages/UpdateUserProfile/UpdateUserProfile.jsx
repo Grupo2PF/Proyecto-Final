1da6d1e56ce58c3d4d6865a4f068b26a39d0ed49
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-12-01 15:42:56 -0400

    Create UpdateUserProfile Page

---
+++ front/src/pages/UpdateUserProfile/UpdateUserProfile.jsx
@@ -0,0 +1,9 @@
+import styles from "./UpdateUserProfile.module.scss";
+
+export default function UpdateUserProfile() {
+  return (
+    <section>
+      <h2>UpdateUserProfile</h2>
+    </section>
+  );
+}
