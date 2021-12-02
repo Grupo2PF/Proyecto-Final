bd4adb381c6dc657593fbd2d956f99bc896b83ac
Esteban Luna <estebanlun14777@gmail.com>
2021-12-01 21:17:21 -0300

    Lista de resultados terminadada. Falta boton de Fav y poder añadir a compras

--- front/src/pages/OfferPage/OfferPage.tsx
+++ front/src/pages/OfferPage/OfferPage.tsx
@@ -1,92 +1,22 @@
 import styles from "./OfferPage.module.scss";
-// import info from "./OfferJson";
 import { useSelector } from "react-redux";
-// import { useState } from "react";
-import OfferCard from "../../components/OfferCard/OfferCard";
-import { RiH2 } from "react-icons/ri";
-// import { useEffect } from "react";
-
-type transfers = {
-  id?: string;
-  origin: string;
-  destination: string;
-  departure: string;
-  arrive: string;
-  airline: string;
-  flightNumber: string;
-};
-
-type offer = {
-  id: string;
-  price: string;
-  currency: string;
-  airline: string;
-  departure: {
-    id: string;
-    origin: {
-      city: string;
-      airport: string;
-      date: string;
-    };
-    destiny: {
-      city: string;
-      airport: string;
-    };
-    transfers: Array<transfers>;
-  };
-  return?: {
-    id: string;
-    origin: {
-      city: string;
-      airport: string;
-      date: string;
-    };
-    destiny: {
-      city: string;
-      airport: string;
-    };
-    transfers: Array<transfers>;
-  };
-};
-
-export type Back = {
-  mode: string;
-  class: string;
-  origin: {
-    city: string;
-    airport: string;
-  };
-  destination: {
-    city: string;
-    airport: string;
-  };
-  offers: Array<offer>;
-};
+import OfferCardI from "../../components/OfferCard/OfferCardI";
+import OfferCardIV from "../../components/OfferCard/OfferCardIV";
 
 export default function OfferPage(): JSX.Element {
-  // const dispatch = useDispatch();
 
   const response: any = useSelector((state: any) => state.allFlight);
 
-  console.log(" soy el response del useSelector ");
-  console.log(response);
-
+  
   return (
     <section className={styles.divContainer}>
       <header>
-
-        {response ? <h2>{response.origin.airport} </h2> : <h2>Nada de nada </h2>}
-
-
-        {response
-          ? <h2>{response.destination.airport}</h2>
-          : <h2>Nada de nada </h2>}
-
+        <h2> {response.origin.airport ? response.origin.airport : response.origin.city } - {response.destination.airport ? response.destination.airport : response.destination.city } </h2>
       </header>
-      <h3>Holaaso?</h3>
-      {response ?
+
+      {response.mode === "oneway" ?
         response.offers.map((item: any) => (
-          <OfferCard
+          <OfferCardI
             offers={item.id}
             currency={item.currency}
             price={item.price}
@@ -96,10 +26,23 @@ export default function OfferPage(): JSX.Element {
             destinationCity={response.destination.city}
             originAirport={response.origin.airport}
             destinationAirport={response.destination.airport}
-          />
-        ))
+          />))
+          : 
+        response.offers.map((item: any) => (
+          <OfferCardIV
+            offers={item.id}
+            currency={item.currency}
+            price={item.price}
+            transfersD={item.departure.transfers}
+            transfersR={item.return.transfers}
+            mode={response.mode}
+            originCity={response.origin.city}
+            destinationCity={response.destination.city}
+            originAirport={response.origin.airport}
+            destinationAirport={response.destination.airport}
+          />))
+        }
 
-        : <h2>No detecta el response</h2>}
     </section>
-  );
+   )
 }
